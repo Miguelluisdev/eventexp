c4156df39fd87f40eeab11f81496afacce472ec6
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-07-25 21:20:14 -0300

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <link rel="icon" href="/favicon.png" sizes="any" />
     <head />
     <body className={inter.className} >
       <ThemeProvider
