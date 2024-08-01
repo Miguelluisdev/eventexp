7f821b8756f88945d6c14e493e80912b79f8c08a
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-07-31 22:21:07 -0300

    refactor: models from db

--- src/lib/database/models/event.model.ts
+++ src/lib/database/models/event.model.ts
@@ -9,12 +9,13 @@ export interface IEvent extends Document {
   imageUrl: string
   startDateTime: Date
   endDateTime: Date
-  price?: string
+  price: string
   isFree: boolean
   url?: string
-  category?: { _id: string; name: string }
-  organizer?: { _id: string; firstName: string; lastname: string }
+  category: { _id: string; name: string }
+  organizer: { _id: string; firstName: string; lastName: string }
 }
+
 const EventSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String },
