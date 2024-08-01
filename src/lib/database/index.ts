a8798e065e31a0d95222ea394c324c4346e4fbac
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-07-31 21:03:36 -0300

    refactor: names for db

--- src/lib/database/index.ts
+++ src/lib/database/index.ts
@@ -1,15 +1,15 @@
 import mongoose from "mongoose"
 
-const MONGODB_URL = process.env.MONGODB_URL
+const MONGODB_URI = process.env.MONGODB_URI
 
 let cached = (global as any).mongoose || { conn: null, promise: null }
 
-export const connectDatabase = async () => {
+export const connectToDatabase = async () => {
     if(cached.conn) return cached.conn
 
-    if(!MONGODB_URL) throw new Error("mongodb não existe")
+    if(!MONGODB_URI) throw new Error("mongodb não existe")
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL , {
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI , {
         dbName: "eventexp",
         bufferCommands: false
     })
